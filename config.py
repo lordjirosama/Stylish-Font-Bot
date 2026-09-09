97161a3db09153e1f24d4dcbb8b84a906b21de32
lordjirosama <lordjiro615@gmail.com>
2026-09-10 02:03:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ PICS = (os.environ.get("PICS", "https://i.ibb.co/MDssddJp/pic.jpg https://i.ibb.
 ADMIN = int(os.environ.get("ADMIN", "7754709357"))
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1004422224007"))
 DB_URI = os.environ.get("DB_URI", "mongodb+srv://tgjiro441_db_user:Tgjirosamkun@cluster0.9reqco1.mongodb.net/?appName=Cluster0")
-DB_NAME = os.environ.get("DB_NAME", "")
+DB_NAME = os.environ.get("DB_NAME", "stylish_font_bot")
 IS_FSUB = os.environ.get("IS_FSUB", "False").lower() == "true"  # Set "True" For Enable Force Subscribe
 AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNELS", "").split())) # Add Multiple channel ids
 AUTH_REQ_CHANNELS = list(map(int, os.environ.get("AUTH_REQ_CHANNELS", "").split())) # Add Multiple channel ids
